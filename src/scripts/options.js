7bb167e9a94fec0b8f4360333182d9a63f80f52a
edencore <edencore@gmail.com>
2014-04-22 19:39:27 +0400

    some fixes and saving\loading has been restored

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -9,26 +9,24 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  reloadAlbumList();
-
   $("#patterns").dblclick(removePattern);
 
   $("#albums").dblclick(function (ev) {
-    addPattern(t);
+    addPattern(ev.srcElement.innerText);
   });
 
   $("#pattern-input").keydown(function (ev) {
     if (ev.which == 13) {
-        addPattern($(this).val());
-        $(this).val("");
+      addPattern($(this).val());
+      $(this).val("");
     }
   });
 
   $("input[type=radio]").click(saveListType);
-
+  load();
 });
 
-function addPattern(ev) {
+function addPattern(pt) {
   $("#patterns").append($("<option></option>").attr("value", pt).text(pt));
   showMatched();
   save();
@@ -43,37 +41,38 @@ function removePattern(ev) {
 /*
  * Getting list of albums from picasa's user profile
  */
-function getAlbums() {
+function buildAlbums(callback) {
   var bp = chrome.extension.getBackgroundPage(),
       $list = $("#albums");
   bp.reloadAlbums(function (albums) {
     for (var i = 0, max = albums.length; i < max; i++) {
       $list.append($("<option></option>").attr("value", albums[i].title).text(albums[i].title));
     };
+    if (callback && typeof callback == 'function')
+      callback();
   });
 }
 
 function reloadAlbumList() {
 	$("#albums")[0].innerHTML = "";
-	getAlbums();
+	buildAlbums(showMatched);
 };
 
 function showMatched() {
-	var result = $("#matched");
-	result.text("");
+  var result = $("#matched");
+  result.text("");
 
-	var patterns = $("#patterns")[0].children;
-	var all = $("#albums")[0].children;
+  var patterns = $("#patterns")[0].children;
+  var all = $("#albums")[0].children;
 
-	for (var i=0; i<patterns.length; i++) {
-		var reg = new RegExp(patterns[i].innerText);
-		for (var x=0; x<all.length; x++) {
-			var el = all[x];
-			if (reg.test(all[x].text))
-				result.append(all[x].text + "<br/>");
-		};
-	};
-	chrome.extension.getBackgroundPage().getAlbums();
+  for (var i=0; i<patterns.length; i++) {
+    var reg = new RegExp(patterns[i].innerText);
+    for (var x = 0; x < all.length; x++) {
+      var el = all[x];
+      if (reg.test(all[x].text))
+          result.append(all[x].text + "<br/>");
+    };
+  };
 }
 
 function save() {
@@ -93,24 +92,24 @@ function saveListType() {
 }
 
 function loadListType() {
-    chrome.storage.sync.get("isblack", function (it) {
-        if (it.isblack)
-            $("#black").attr("checked", "checked");
-        else
-            $("#white").attr("checked", "checked");
-    });
+  chrome.storage.sync.get("isblack", function (it) {
+    if (it.isblack)
+        $("#black").attr("checked", "checked");
+    else
+        $("#white").attr("checked", "checked");
+  });
 }
 
 function load() {
-	var list = $("#patterns");
-	list.text("");
-	chrome.storage.sync.get("patterns", function (items) {
-		var pts = items.patterns;
+  var list = $("#patterns");
+  list.text("");
+  chrome.storage.sync.get("patterns", function (items) {
+    var pts = items.patterns;
 
-		for (var i=0; i<pts.length; i++) {
-			list.append($("<option></option>").attr("value", pts[i]).text(pts[i]));
-		};
-		showMatched();
-	});
-	loadListType();
+    for (var i = 0; i < pts.length; i++) {
+      list.append($("<option></option>").attr("value", pts[i]).text(pts[i]));
+    };
+    reloadAlbumList();
+  });
+  loadListType();
 }
